fc35eecbb83f643c51669f2c1693d8d085f1d028
alyssa1108 <alyssamoodley9@gmail.com>
2026-06-18 17:55:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -21,59 +21,80 @@ unlockBtn.addEventListener("click", () => {
     if(passwordInput.value === "JoashManicum"){
 
         errorMsg.innerText = "";
-
         book.classList.add("open");
 
         setTimeout(() => {
             document.getElementById("scrapbook-unlock").style.display = "none";
             document.getElementById("scrapbook").style.display = "flex";
-        }, 900);
+        }, 800);
 
     } else {
         errorMsg.innerText = "Wrong Password ❤️";
     }
 });
 
-/* DATES */
+/* =========================
+   EDIT YOUR DATES HERE
+   (THIS IS WHAT YOU CAN CHANGE ANYTIME)
+========================= */
+
 const dates = [
-"01 Jan 2025","05 Jan 2025","10 Jan 2025","15 Jan 2025","20 Jan 2025",
-"25 Jan 2025","01 Feb 2025","05 Feb 2025","10 Feb 2025","14 Feb 2025",
-"20 Feb 2025","25 Feb 2025","01 Mar 2025","05 Mar 2025","10 Mar 2025"
+"01 Jan 2025",
+"05 Jan 2025",
+"10 Jan 2025",
+"15 Jan 2025",
+"20 Jan 2025",
+"25 Jan 2025",
+"01 Feb 2025",
+"05 Feb 2025",
+"10 Feb 2025",
+"14 Feb 2025",
+"20 Feb 2025",
+"25 Feb 2025",
+"01 Mar 2025",
+"05 Mar 2025",
+"10 Mar 2025"
 ];
 
-/* PAGES */
+/* =========================
+   15 UNIQUE PAGES ONLY
+   (NO REPEATS)
+========================= */
+
 const pages = [];
 
-for(let i=1;i<=30;i++){
-    let imgNum = i <= 15 ? i : i-15;
-
+for(let i=1;i<=15;i++){
     pages.push({
-        img:`pic0${imgNum}.jpg.jpeg`,
-        date:dates[(i-1)%dates.length]
+        img:`pic0${i}.jpg.jpeg`,
+        date:dates[i-1] || ""
     });
 }
 
 let currentPage = 0;
-let notes = new Array(30).fill("");
+let notes = new Array(15).fill("");
 
+/* RENDER PAGE */
 function renderPage(){
     photo.src = pages[currentPage].img;
     dateText.innerText = pages[currentPage].date;
-    pageNum.innerText = `${currentPage+1} / 30`;
+    pageNum.innerText = `${currentPage+1} / 15`;
     noteBox.value = notes[currentPage];
 }
 
+/* SAVE TEXT PER PAGE */
 noteBox.addEventListener("input", () => {
     notes[currentPage] = noteBox.value;
 });
 
+/* NEXT */
 nextBtn.addEventListener("click", () => {
-    if(currentPage < 29){
+    if(currentPage < 14){
         currentPage++;
         renderPage();
     }
 });
 
+/* PREV */
 prevBtn.addEventListener("click", () => {
     if(currentPage > 0){
         currentPage--;
@@ -81,9 +102,10 @@ prevBtn.addEventListener("click", () => {
     }
 });
 
+/* INIT */
 renderPage();
 
-/* sparkles */
+/* SPARKLES */
 document.addEventListener("click", e => {
     for(let i=0;i<10;i++){
         const sparkle = document.createElement("div");
