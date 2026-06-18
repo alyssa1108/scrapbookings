62c04850d089fa5eac2bd67581de9d81cf654d52
alyssa1108 <alyssamoodley9@gmail.com>
2026-06-18 17:36:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,67 +1,33 @@
-const book =
-document.getElementById("book");
-
-const unlockBtn =
-document.getElementById("unlockBtn");
-
-const passwordInput =
-document.getElementById("passwordInput");
-
-const errorMsg =
-document.getElementById("errorMsg");
+const book = document.getElementById("book");
+const unlockBtn = document.getElementById("unlockBtn");
+const passwordInput = document.getElementById("passwordInput");
+const errorMsg = document.getElementById("errorMsg");
 
 book.addEventListener("click", () => {
-
-if(!book.classList.contains("open")){
-
-book.classList.add("open");
-
-}
-
+    if(!book.classList.contains("open")){
+        book.classList.add("open");
+    }
 });
 
 unlockBtn.addEventListener("click", () => {
-
-if(passwordInput.value === "JoashManicum"){
-
-alert(
-"Password Correct! Next we build the scrapbook pages."
-);
-
-}else{
-
-errorMsg.innerText =
-"Wrong Password ❤️";
-
-}
-
+    if(passwordInput.value === "JoashManicum"){
+        alert("Password Correct! Next we build the scrapbook pages.");
+    } else {
+        errorMsg.innerText = "Wrong Password ❤️";
+    }
 });
 
 document.addEventListener("click", e => {
+    for(let i=0;i<10;i++){
+        const sparkle = document.createElement("div");
+        sparkle.classList.add("sparkle");
 
-for(let i=0;i<10;i++){
+        sparkle.style.left = e.pageX + "px";
+        sparkle.style.top = e.pageY + "px";
 
-const sparkle =
-document.createElement("div");
+        document.body.appendChild(sparkle);
 
-sparkle.classList.add("sparkle");
-
-sparkle.style.left =
-e.pageX + "px";
-
-sparkle.style.top =
-e.pageY + "px";
-
-document.body.appendChild(sparkle);
-
-setTimeout(() => {
-
-sparkle.remove();
-
-},1000);
-
-}
-
+        setTimeout(() => sparkle.remove(), 1000);
+    }
 });
-
                  
